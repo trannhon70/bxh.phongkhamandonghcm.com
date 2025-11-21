85494da5f94d1d5bac4909eac8cc4038ba5dd8a0
trannhon70 <kevintran351996@gmail.com>
2025-11-21 09:54:30 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -39,8 +39,8 @@ $dbReadTotalTime = ($dbReadEndTime - $dbReadStarTime) / 1e+6;
 
 header('Server-Timing: db;desc="Database";dur=' . $dbReadTotalTime);
 
-// $local = 'http://localhost/_andong/bxh.phongkhamandonghcm.com'
-$local = 'https://bxh.phongkhamandonghcm.com'
+$local = 'http://localhost/_andong/bxh.phongkhamandonghcm.com'
+// $local = 'https://bxh.phongkhamandonghcm.com'
 ?>
 <!DOCTYPE html>
 <html ⚡ lang="en">
@@ -153,3 +153,21 @@ $local = 'https://bxh.phongkhamandonghcm.com'
             document.removeEventListener('scroll', loadGA);
         });
     </script>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PX2JGH6D');
+    </script>
+    <!-- End Google Tag Manager -->
